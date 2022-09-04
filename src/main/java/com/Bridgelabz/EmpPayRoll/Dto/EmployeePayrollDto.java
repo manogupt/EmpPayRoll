b29120cb43ae5b9f9798b13bc7e6358ff70ece50
manogupt <manojgupta123929@gmail.com>
2022-09-04 21:26:31 +0530

    Added : add validations to name

--- src/main/java/com/Bridgelabz/EmpPayRoll/Dto/EmployeePayrollDto.java
+++ src/main/java/com/Bridgelabz/EmpPayRoll/Dto/EmployeePayrollDto.java
@@ -1,10 +1,13 @@
 package com.Bridgelabz.EmpPayRoll.Dto;
 
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 
 @Data
 public class EmployeePayrollDto {
 //	private long id;
+	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="name invalid")
 	private String name;
 	private double salary;
 	
